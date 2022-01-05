5f9f6d090f10f6d1c42770d886e63b110184546e
ShubhamSj07 <shubhamsj077@gmail.com>
2022-01-05 13:43:02 +0530

    testing lint-staged 3

---
+++ src/Child.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Child = () => {
+    return (
+        <div>
+           <h1>testing lint staged for unstaged files:)</h1> 
+        </div>
+    )
+}
+
+export default Child
