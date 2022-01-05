236065c343113fb41eddb43371f7ad9c4f5cf8c5
ShubhamSj07 <shubhamsj077@gmail.com>
2022-01-05 13:44:21 +0530

    testing lint-staged 4

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,11 @@ import "./App.css";
 const App = () => {
   return (
     <div>
-      
+
+
         <h3>react-typescript boilerplate!</h3>
 <h5>testing lint stage!!</h5>
+
       <img src={FaSun} alt="FaSun" />
     </div>
   );
--- src/Child.tsx
+++ src/Child.tsx
@@ -4,6 +4,8 @@ const Child = () => {
     return (
         <div>
            <h1>testing lint staged for unstaged files:)</h1> 
+
+           
         </div>
     )
 }
